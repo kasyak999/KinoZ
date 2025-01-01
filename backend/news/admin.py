5010e050a1e49e28850c12473c661840428b116e
Максим <kasyak999@yandex.ru>
2025-01-02 00:10:44 +0300

    up

--- backend/news/admin.py
+++ backend/news/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.conf import settings
+from .models import EventUser
 
-# Register your models here.
+
+@admin.register(EventUser)
+class ComentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'event',
+        'user',
+        'created_at',
+    )
+    list_display_links = ('user',)
+    list_per_page = settings.OBJECTS_PER_PAGE
--- backend/news/models.py
+++ backend/news/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+EVENT_CHOICES = [
+    ('add_film', 'добавил фильм в избранное'),
+    ('del_film', 'удалил фильм в избранное'),
+]
+
+
+class EventUser(models.Model):
+    """Подписки пользователей"""
+    event = models.CharField(
+        max_length=256, verbose_name='Событие', choices=EVENT_CHOICES)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='Добавлено'
+    )
+
+    class Meta:
+        """Перевод модели"""
+        verbose_name = 'событие'
+        verbose_name_plural = 'События'
+        ordering = ('created_at',)
+
+    def __str__(self):
+        return f'Событие {self.user}'
