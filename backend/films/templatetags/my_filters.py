c716e7202e7b189eacb44c0b96483dbb5722611c
Максим <kasyak999@yandex.ru>
2025-01-07 23:38:00 +0300

    up

--- backend/films/templatetags/my_filters.py
+++ backend/films/templatetags/my_filters.py
@@ -1,6 +1,6 @@
+import ast
 import json
 from django import template
-import ast
 
 
 register = template.Library()
@@ -8,44 +8,30 @@ register = template.Library()
 
 @register.filter
 def split(value):
-    """Фильтр, который разделяет строку по запятой."""
+    """Фильтр, создает список."""
     return value.split(', ')
 
 
 @register.filter
 def numeric(value):
     """Фильтр, который разделяет число по тысячам."""
-    value = f"{value:,}".replace(',', ' ')
-    return value
+    return f"{value:,}".replace(',', ' ')
 
 
 @register.filter
 def json_to_list(value):
     """Преобразует строку JSON в список."""
-    try:
-        return json.loads(value)
-    except (ValueError, TypeError):
-        return []
+    return json.loads(value)
 
 
 @register.filter
-def to_dict(value):
+def str_to_dict(value):
     """Фильтр преобразует строку в словарь"""
-    try:
-        result = ast.literal_eval(value)
-        if isinstance(result, dict):
-            return result.items()
-    except (ValueError, SyntaxError):
-        pass
-    return []
+    result = ast.literal_eval(value)
+    return result.items()
 
 
 @register.filter
-def join_list(value, delimiter=", "):
-    """
-    Преобразует список в строку с указанным разделителем.
-    Если значение не список, возвращает исходное значение.
-    """
-    if isinstance(value, list):
-        return delimiter.join(str(item) for item in value)
-    return value
+def list_to_str(value):
+    """Преобразует список в строку"""
+    return str.join(', ', value)
