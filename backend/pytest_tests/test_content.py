2fd925ef853eb98ef4397f192eefe94244479e61
Максим <kasyak999@yandex.ru>
2025-01-18 23:04:46 +0300

    up

--- backend/pytest_tests/test_content.py
+++ backend/pytest_tests/test_content.py
@@ -1,16 +1,24 @@
 import pytest
-from http import HTTPStatus
-from pytest_django.asserts import assertRedirects
+# from http import HTTPStatus
+# from pytest_django.asserts import assertRedirects
 from django.urls import reverse
-from pprint import pprint
+from films.form import ComentForm, FilmLinkForm, FormComment
+from users.form import MessageForm
 
 
 @pytest.fixture
-def urls():
+def urls(author, films_true, not_author, comments_add):
     """Фикстура для url"""
     return {
         'index': reverse('films:index'),
         'favorite': reverse('films:favorite'),
+        'user': reverse('users:user', args=[author.username]),
+        'film': reverse('films:film', args=[films_true.id_kp]),
+        'message': reverse('users:message_username', args=[
+            not_author.username]),
+        'add_film': reverse('films:add_film'),
+        'edit_comment': reverse('films:edit_comment', args=[
+            films_true.id_kp, comments_add.id]),
     }
 
 
@@ -65,3 +73,35 @@ def test_favorite_films(favorite, urls, avtar, status, request):
     assert (favorite.film in object_list) is status, (
         'Фильм отсутствует в избранном списке пользователя'
         ' или присутствует у других')
+
+
+@pytest.mark.parametrize(
+    'url',
+    ('film', 'user')
+)
+@pytest.mark.django_db
+def test_user_comments(comments_add, url, urls, author_client):
+    """Есть ли комментарии пользователя на странице пользователя"""
+    response = author_client.get(urls[url])
+    object_list = response.context['object_list']
+    assert comments_add in object_list, (
+        'Отсутствуют комментарии пользователя')
+
+
+@pytest.mark.parametrize(
+    'url, form',
+    (
+        ('film', ComentForm),
+        ('message', MessageForm),
+        ('add_film', FilmLinkForm),
+        ('edit_comment', FormComment)
+    )
+)
+@pytest.mark.django_db
+def test_qwe(films_true, url, form, urls, author_client):
+    """Проверка форм"""
+    response = author_client.get(urls[url])
+    assert 'form' in response.context, f'Нет формы на странице {url}'
+    assert isinstance(response.context['form'], form), (
+        f'Форма не пренадлежит к классу {form}'
+    )
